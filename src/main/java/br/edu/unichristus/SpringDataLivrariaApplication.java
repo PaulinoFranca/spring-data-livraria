03c4a1a3a257edc44674282896e3e3fc22bf4e74
Aluno <Aluno>
2017-02-09 21:51:28 -0300

    Criacao de camadas

--- src/main/java/br/edu/unichristus/SpringDataLivrariaApplication.java
+++ src/main/java/br/edu/unichristus/SpringDataLivrariaApplication.java
@@ -1,10 +1,23 @@
 package br.edu.unichristus;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataLivrariaApplication {
+public class SpringDataLivrariaApplication implements CommandLineRunner {
+	@Autowired
+	private LivroService servicoLivros;
+
+	@Override
+	public void run(String... args) {
+		Livro livro = new Livro("Java Como Programar", new Date(), 250, new BigDecimal("15.00"));
+		this.servicoLivros.salvar(livro);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataLivrariaApplication.class, args);
